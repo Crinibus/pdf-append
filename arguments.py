31502bdaabfd9ac6576511b84f4a5de2c85b0189
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-03-13 00:56:30 +0100

    Update arguments.py
    - Remove 'required' parameter for arguments, instead add function "validate_arguments"
    - Rename 'dest' parameter for argument --page from "page_index" to "page_indexes"
    - Add argument --remove

--- arguments.py
+++ arguments.py
@@ -12,7 +12,6 @@ def argparse_setup() -> ArgumentParser.parse_args:
         metavar='<filename>',
         help='the output pdf file to create or append pages to',
         type=str,
-        required=True,
     )
 
     parser.add_argument(
@@ -22,17 +21,15 @@ def argparse_setup() -> ArgumentParser.parse_args:
         metavar='<filename>',
         help='the input pdf file to take pages from',
         type=str,
-        required=True,
     )
 
     parser.add_argument(
         '-p',
         '--page',
-        dest='page_index',
+        dest='page_indexes',
         metavar='<page_index>',
         help='index of pages to append to output file',
         type=int,
-        required=True,
         nargs='*',
     )
 
@@ -44,4 +41,30 @@ def argparse_setup() -> ArgumentParser.parse_args:
         action='store_true',
     )
 
+    parser.add_argument(
+        '-r',
+        '--remove',
+        dest='remove',
+        help='remove pages from input pdf file',
+        action='store_true',
+    )
+
+    validate_arguments(parser)
+
     return parser.parse_args()
+
+
+def validate_arguments(parser: ArgumentParser) -> None:
+    """Validate arguments"""
+    args = parser.parse_args()
+
+    if args.remove:
+        if not args.input_file or not args.page_indexes:
+            parser.error("When using --remove, then --input and --page is required")
+    elif args.create:
+        if not args.output_file:
+            parser.error("When using --create, then --output is required")
+    else:
+        if args.output_file:
+            if not args.input_file or not args.page_indexes:
+                parser.error("When using --output, then both --input and --page is required")
