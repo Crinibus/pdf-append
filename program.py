35f1e567e76cba3308164b6c582e78e485543440
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-03-13 00:58:50 +0100

    Update program.py
    - Add function "write_pdf"
    - Add function "remove_pages_from_pdf", called when flag --remove is used

--- program.py
+++ program.py
@@ -16,8 +16,7 @@ def add_page_to_pdf(page_index: int, input_pdf_name: str, output_pdf_name: str):
 
     pdf_writer.addPage(pdf.getPage(page_index-1))
 
-    with open(output_pdf_name, "wb") as out_pdf:
-        pdf_writer.write(out_pdf)
+    write_pdf(output_pdf_name, pdf_writer)
 
     print(f"Added page {page_index} from pdf {input_pdf_name} to pdf {output_pdf_name}")
 
@@ -30,10 +29,23 @@ def append_pages_to_pdf(page_indexes: List[int], input_pdf_name: str, output_pdf
     for index in page_indexes:
         pdf_writer.addPage(pdf.getPage(index-1))
 
-    with open(output_pdf_name, "wb") as out_pdf:
-        pdf_writer.write(out_pdf)
+    write_pdf(output_pdf_name, pdf_writer)
 
-    print(f"Appended pages from {input_pdf_name} to {output_pdf_name}")
+    print(f"Appended page(s) from {input_pdf_name} to {output_pdf_name}")
+
+
+def remove_pages_from_pdf(page_indexes: List[int], pdf_name: str):
+    pdf = PdfFileReader(pdf_name)
+
+    pdf_writer = PdfFileWriter()
+
+    for index in range(pdf.getNumPages()):
+        if index+1 not in page_indexes:
+            pdf_writer.addPage(pdf.getPage(index))
+    
+    write_pdf(pdf_name, pdf_writer)
+
+    print(f"Removed page(s) from {pdf_name}")
 
 
 def get_all_pages(pdf_name: str) -> PdfFileWriter:
@@ -46,10 +58,18 @@ def get_all_pages(pdf_name: str) -> PdfFileWriter:
     return pdf_writer
 
 
+def write_pdf(filename: str, pdf_writer: PdfFileWriter):
+    with open(filename, "wb") as out_pdf:
+        pdf_writer.write(out_pdf)
+
+
 if __name__ == "__main__":
     args = argparse_setup()
 
     if args.create:
         create_pdf(args.output_file)
 
-    append_pages_to_pdf(args.page_index, args.input_file, args.output_file)
+    if args.remove:
+        remove_pages_from_pdf(args.page_indexes, args.input_file)
+    else:
+        append_pages_to_pdf(args.page_indexes, args.input_file, args.output_file)
